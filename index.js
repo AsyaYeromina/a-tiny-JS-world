2b941c01051d1da3717bb0ac3935aa7d6b974e52
Asya Yeromina <asyayeromina@gmail.com>
2020-12-30 16:09:16 +0300

    Making world from scratch vers with classes

--- index.js
+++ index.js
@@ -1,97 +1,64 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+  Complete the below for code reviewers' convenience:
 
-   Code repository: https://github.com/AsyaYeromina/a-tiny-JS-world
-   Web app: https://asyayeromina.github.io/a-tiny-JS-world/.
-   */
+  Code repository: https://github.com/AsyaYeromina/a-tiny-JS-world
+  Web app: https://asyayeromina.github.io/a-tiny-JS-world/.
+  */
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-const dog = {
-   species: 'Canis',
-   name: 'Jessie',
-   gender: 'female',
-   legs: '4',
-   hands: '0',
-   speech: 'woof-woof!',
-   friends: ['Stepan', 'Ivanka']
-};
+class Inhabitant {
+  constructor(species, name, gender, legs, hands, speech, friends){
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.legs = legs;
+    this.hands = hands;
+    this.speech = speech;
+    this.friends = friends;
+  }
 
-const cat = {
-   species: 'Felis catus',
-   name: 'Python',
-   gender: 'male',
-   legs: '4',
-   hands: '0',
-   speech: 'Meow',
-   friends: []
-};
-
-const woman = {
-   species: 'Homo sapiens',
-   name: 'Ivanka',
-   gender: 'female',
-   legs: '2',
-   hands: '2',
-   speech: 'Hello!',
-   friends: ['Stepan', 'Python', 'Jessie']
-};
-
-const man = {
-   species: 'Homo sapiens',
-   name: 'Stepan',
-   gender: 'male',
-   legs: '2',
-   hands: '2',
-   speech: 'Hello!',
-   friends: ['Ivanka', 'Python', 'Jessie']
-};
-
-const catWoman = {
-   species: 'Felis sapiens',
-   name: 'Selina',
-   gender: 'female',
-   legs: '2',
-   hands: '2',
-   speech: cat.speech,
-   friends: ['Stepan', 'Python']
-};
-
-const inhabitants = [cat, dog, woman, man, catWoman];
-const keys = ['species', 'name', 'gender', 'legs', 'hands', 'speech', 'friends'];
-
-const getValues = function(obj, arr) {
-   const stringArr = [];
-   arr.forEach(key => {
-      const value = obj[key];
-      if (typeof value === 'object') {
-         stringArr.push(value.join(', '))
-      } else
-      stringArr.push(value);
-   });
-   return stringArr.join('; ');
+  makeString(){
+    return [this.species, this.name, this.gender, this.legs, this.hands, this.speech, this.friends]
+    .map(value => Array.isArray(value) ? value.join(", ") : value)
+    .join("; ")
+  }
 }
 
-inhabitants.map(inhabitant => {
-   print(getValues(inhabitant, keys))
+class Animal extends Inhabitant {
+}
+
+class Sapiens extends Inhabitant {
+}
+
+
+const dog = new Animal('Canis', 'Jessie', 'female', '4', '0', 'Woof-woof!', ['Stepan', 'Ivanka']);
+const cat = new Animal('Felis catus', 'Python', 'male', '4', '0', 'Meow', []);
+const woman = new Sapiens('Homo sapiens', 'Ivanka', 'female', '2', '2', 'Hello!', ['Stepan', 'Python', 'Jessie']);
+const man = new Sapiens('Homo sapiens', 'Stepan', 'male', '2', '2', 'Hello!', ['Ivanka', 'Python', 'Jessie']);
+const catWoman = new Sapiens('Felis sapiens', 'Selina', 'female', '2', '2', cat.speech, ['Stepan', 'Python']);
+const inhabitantObjects = [dog, cat, woman, man, catWoman];
+
+  inhabitantObjects.map(inhabitant => {
+    print(inhabitant.makeString())
 })
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+  Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+  Message can contain HTML markup. You may also tweak index.html and/or styles.css.
+  However, please, REFRAIN from improving visuals at least until your code is reviewed
+  so code reviewers might focus on a single file that is index.js.
+  */
 
 /* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+  print('ABC');
+  print('<strong>ABC</strong>');
+  print('<strong>ABC</strong>', 'div');
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+  print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
+  print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
+  print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
+  */
